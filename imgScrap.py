8c41c7b45e028e7f7e00b62fc128cf093bbc54fe
Joflesan <s2088605@ed.ac.uk>
2021-08-16 20:39:51 +0100

    Tweaked Bot Implementation

--- imgScrap.py
+++ imgScrap.py
@@ -5,6 +5,7 @@ Google Image Python Scraper
 @author: josflesan (github.com/josflesan)
 """
 
+
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
 
@@ -34,7 +35,31 @@ class GoogleImgScrapy:
         return f"https://www.google.com/search?safe=off&site=&tbm=isch&source=hp&q={query}&oq={query}&gs_l=img"
 
     def _get_info(self, query: str):
-        image_urls = set()
+        image_url = ''
+
+        self.driver.get(self._build_query(query))
+        self._scroll_to_end()
+
+        # img.Q4LuWd is google's thumbnail selector
+        thumbnail = self.driver.find_element_by_css_selector("img.Q4LuWd")
+
+        print(f"Getting the link...")
+
+        try:
+            thumbnail.click()  # Click on the first image
+        except NoSuchElementException:
+            print('ERROR: Cannot click on image')
+
+        image = self.driver.find_element_by_class_name('n3VNCb')  # Select image pop up
+        time.sleep(0.5)
+
+        if image.get_attribute('src') and 'http' in image.get_attribute('src'):
+            image_url = image.get_attribute('src')
+
+        return image_url
+
+    def _get_mult_info(self, query: str, num_images: int):
+        image_urls = []
 
         self.driver.get(self._build_query(query))
         self._scroll_to_end()
@@ -42,25 +67,25 @@ class GoogleImgScrapy:
         # img.Q4LuWd is google's thumbnail selector
         thumbnails = self.driver.find_elements_by_css_selector("img.Q4LuWd")
 
-        print(f"Found {len(thumbnails)} images...")
         print(f"Getting the links...")
 
-        try:
-            thumbnails[0].click()  # Click on the first image
-        except NoSuchElementException:
-            print('ERROR: Cannot click on image')
+        for thumbnail in thumbnails[0:num_images+1]:
+            try:
+                thumbnail.click()  # Click on the first image
 
-        images = self.driver.find_elements_by_class_name('n3VNCb')  # Select image pop up
-        time.sleep(0.3)
+                images = self.driver.find_elements_by_class_name('n3VNCb')  # Select image pop up
+                time.sleep(0.3)
 
-        if images[0].get_attribute('src') and 'http' in images[0].get_attribute('src'):
-            image_urls.add(images[0].get_attribute('src'))
+                for image in images:
+                    if image.get_attribute('src') and 'http' in image.get_attribute('src'):
+                        image_urls.append(image.get_attribute('src'))
+
+            except NoSuchElementException:
+                print('ERROR: Cannot click on image')
 
         return image_urls
 
-    def download_image(self, folder_path: str, url: str):
-
-        num = "01"
+    def download_image(self, query: str, folder_path: str, url: str):
 
         try:
             image_content = requests.get(url).content
@@ -71,23 +96,48 @@ class GoogleImgScrapy:
         try:
             image_file = io.BytesIO(image_content)
             image = Image.open(image_file).convert('RGB')
-            file = os.path.join(folder_path, num + '.jpg')
+            file = os.path.join(folder_path, query + '.jpg')
 
             with open(file, 'wb') as f:
                 image.save(f, "JPEG", quality=85)
+            time.sleep(0.3)
             print(f"SUCCESS: saved {url} - as {file}")
 
         except Exception as e:
             print(f"ERROR: Could not save {url} - {e}")
 
-    def scrape_images(self, query: str, folder_path: str):
+    def scrape_single_images(self, queryList: list, folder_path: str):
         folder = os.path.abspath(folder_path)
+        image_urls = []
 
         if not os.path.exists(folder):
             os.makedirs(folder)
 
-        image_info = self._get_info(query)
-        print(f"Downloading images...")
+        for query in queryList:
+            image_url = self._get_info(query)
+            image_urls.append(image_url)
 
-        for image in image_info:
-            self.download_image(folder, image)
+        print(f"Downloading image...")
+
+        for query, url in zip(queryList, image_urls):
+            self.download_image(query, folder, url)
+
+    def scrape_many_images(self, queryList: list, folder_path: str, num_images: int):
+        folder = os.path.abspath(folder_path)
+        image_urls = {}
+        queryPos = 0
+
+        if not os.path.exists(folder):
+            os.makedirs(folder)
+
+        for query in queryList:
+            image_urls[query] = self._get_mult_info(query, num_images)
+
+        print(f"Downloading images...")
+        print(image_urls)
+
+        for query in image_urls.keys():
+            queryPos = 0
+            for url in image_urls[query]:
+                self.download_image(query + str(queryPos), folder, url)
+                queryPos += 1
--- main.py
+++ main.py
@@ -8,11 +8,13 @@ Implementation based on tutorial @ https://rubikscode.net/2021/06/21/scraping-im
 from imgScrap import GoogleImgScrapy
 
 DRIVER_PATH = './webdriver/chromedriver.exe'
+IMG_PATH = 'C:/Users/josue/Documents/Programming/projects/imgScrapy/img'
+QUERIES = ['music']
 
 
 def main():
     goog_scraper = GoogleImgScrapy(DRIVER_PATH)
-    goog_scraper.scrape_images('music', 'C:/Users/josue/Documents/Programming/projects/imgScrapy/img')
+    goog_scraper.scrape_many_images(QUERIES, 'C:/Users/josue/Documents/Programming/projects/imgScrapy/img', 10)
 
 
 if __name__ == "__main__":
